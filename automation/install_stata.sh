bbe18f0e62a31127897645d68e698a0669c22551
Michael Stepner <software@michaelstepner.com>
2022-10-25 14:06:07 -0400

    Make stata version configurable by environment variable

--- automation/install_stata.sh
+++ automation/install_stata.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-version=17
-url_installer=https://d2bx6aas1fcmzl.cloudfront.net/stata_install/Stata${version}Linux64.tar.gz
+# Define parameters
+if [ -z "$STATA_VERSION" ]; then
+    STATA_VERSION=17
+fi
+url_installer=https://d2bx6aas1fcmzl.cloudfront.net/stata_install/Stata${STATA_VERSION}Linux64.tar.gz
 url_license=https://d2bx6aas1fcmzl.cloudfront.net/stata_install/stata.lic
 download_username=oi
 download_password=${OI_HTTPS_PW}
 
 # Install Stata
 mkdir -p /tmp/statafiles
-curl -u ${download_username}:${download_password} ${url_installer} --output /tmp/statafiles/Stata${version}Linux64.tar.gz
+curl -u ${download_username}:${download_password} ${url_installer} --output /tmp/statafiles/Stata${STATA_VERSION}Linux64.tar.gz
 cd /tmp/statafiles
-tar -zxf ./Stata${version}Linux64.tar.gz
-sudo mkdir -p /usr/local/stata${version}
-cd /usr/local/stata${version}
+tar -zxf ./Stata${STATA_VERSION}Linux64.tar.gz
+sudo mkdir -p /usr/local/stata${STATA_VERSION}
+cd /usr/local/stata${STATA_VERSION}
 
 # The following command returns 1 even though it's ok
 set +e
@@ -20,8 +23,8 @@ sudo sh -c 'yes | /tmp/statafiles/install'
 set -e
 
 cd /usr/local/bin
-sudo ln -s /usr/local/stata${version}/stata-mp .
-sudo ln -s /usr/local/stata${version}/xstata-mp .
-sudo curl -u ${download_username}:${download_password} ${url_license} --output /usr/local/stata${version}/stata.lic
+sudo ln -s /usr/local/stata${STATA_VERSION}/stata-mp .
+sudo ln -s /usr/local/stata${STATA_VERSION}/xstata-mp .
+sudo curl -u ${download_username}:${download_password} ${url_license} --output /usr/local/stata${STATA_VERSION}/stata.lic
 rm -r /tmp/statafiles
 cd /tmp
